2d0c52440a11fed44566eb1381b7795e4562cb4e
equinox-jj <joshua.jeremia@hotmail.com>
2026-06-14 02:49:24 +0700

    feat: add AppEmailField and AppPasswordField to core components

---
+++ core/lib/components/text_field/app_email_field.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/validators.dart';
+
+/// A pre-configured email [TextFormField]: email keyboard, email autofill, the
+/// standard mail-icon decoration and [Validators.email] by default.
+class AppEmailField extends StatelessWidget {
+  const AppEmailField({
+    required this.controller,
+    super.key,
+    this.textInputAction = TextInputAction.next,
+    this.onFieldSubmitted,
+    this.validator,
+  });
+
+  final TextEditingController controller;
+  final TextInputAction textInputAction;
+  final ValueChanged<String>? onFieldSubmitted;
+  final FormFieldValidator<String>? validator;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      keyboardType: TextInputType.emailAddress,
+      textInputAction: textInputAction,
+      autofillHints: const <String>[AutofillHints.email],
+      validator: validator ?? Validators.email,
+      onFieldSubmitted: onFieldSubmitted,
+      decoration: const InputDecoration(
+        labelText: 'Email',
+        hintText: 'you@example.com',
+        prefixIcon: Icon(Icons.mail_outline),
+      ),
+    );
+  }
+}
---
+++ core/lib/components/text_field/app_password_field.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/validators.dart';
+
+/// A password [TextFormField] that owns its show/hide state internally,
+/// rendering a visibility toggle in the suffix. Defaults to [Validators.password].
+///
+/// Pass [autofillHints] of `[AutofillHints.password]` for sign-in or
+/// `[AutofillHints.newPassword]` for registration.
+class AppPasswordField extends StatefulWidget {
+  const AppPasswordField({
+    required this.controller,
+    super.key,
+    this.autofillHints = const <String>[AutofillHints.password],
+    this.textInputAction = TextInputAction.done,
+    this.onFieldSubmitted,
+    this.validator,
+  });
+
+  final TextEditingController controller;
+  final List<String> autofillHints;
+  final TextInputAction textInputAction;
+  final ValueChanged<String>? onFieldSubmitted;
+  final FormFieldValidator<String>? validator;
+
+  @override
+  State<AppPasswordField> createState() => _AppPasswordFieldState();
+}
+
+class _AppPasswordFieldState extends State<AppPasswordField> {
+  bool _obscure = true;
+
+  void _toggle() => setState(() => _obscure = !_obscure);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.controller,
+      obscureText: _obscure,
+      textInputAction: widget.textInputAction,
+      autofillHints: widget.autofillHints,
+      validator: widget.validator ?? Validators.password,
+      onFieldSubmitted: widget.onFieldSubmitted,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        prefixIcon: const Icon(Icons.lock_outline),
+        suffixIcon: IconButton(
+          icon: Icon(
+            _obscure ? Icons.visibility_outlined : Icons.visibility_off_outlined,
+          ),
+          onPressed: _toggle,
+        ),
+      ),
+    );
+  }
+}
